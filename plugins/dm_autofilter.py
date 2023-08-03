0e8935db90fc31fae3de488e4adb317a40eeb2de
Sample First <132287607+SampleFirst@users.noreply.github.com>
2023-08-03 23:39:54 +0530

    Update dm_autofilter.py

--- plugins/dm_autofilter.py
+++ plugins/dm_autofilter.py
@@ -206,7 +206,7 @@ async def pm_AutoFilter(client, msg, pmspoll=False):
         btn.append(
             [InlineKeyboardButton(text="📄 𝗣𝗮𝗴𝗲 1/1", callback_data="pages")]
         )
-    if PM_IMDB.strip().lower() in ["true", "yes", "1", "enable", "y"]:
+    if str(PM_IMDB).strip().lower() in ["true", "yes", "1", "enable", "y"]:
         imdb = await get_poster(search)
     else:
         imdb = None
