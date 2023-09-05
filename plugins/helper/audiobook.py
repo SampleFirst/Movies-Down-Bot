60792a810bb7e110f9b5cbe89b690229d76d44d3
Sample First <132287607+SampleFirst@users.noreply.github.com>
2023-09-05 22:58:13 +0530

    Update audiobook.py

--- plugins/helper/audiobook.py
+++ plugins/helper/audiobook.py
@@ -1,52 +1,48 @@
 import os
-import pyrogram
 import PyPDF2
-import time
-from pyrogram import Client, filters, enums
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-from pyrogram.types import User, Message, Document 
+from pyrogram import Client, filters
 from gtts import gTTS
 from info import DOWNLOAD_LOCATION
-  
-Thanks = """ Thats The End Of Your Audio Book, And Thanks for Using this Service"""
 
-@Client.on_message(filters.command(["audiobook"])) # PdfToText 
+# A thank you message
+Thanks = """Thank you for using this service."""
+
+
+@Client.on_message(filters.command(["audiobook"])) 
 async def pdf_to_text(bot, message):
- try:
-           if message.reply_to_message:
-                pdf_path = DOWNLOAD_LOCATION + f"{message.chat.id}.pdf" #pdfFileObject
-                txt = await message.reply("Downloading.....")
-                await message.reply_to_message.download(pdf_path)  
-                await txt.edit("Downloaded File")
-                pdf = open(pdf_path,'rb')
-                pdf_reader = PyPDF2.PdfFileReader(pdf) #pdfReaderObject
-                await txt.edit("Getting Number of Pages....")
-                num_of_pages = pdf_reader.getNumPages() # Number of Pages               
-                await txt.edit(f"Found {num_of_pages} Page")
-                page_no = pdf_reader.getPage(0) # pageObject
-                await txt.edit("Finding Text from Pdf File... ")
-                page_content = """ """ # EmptyString   
-                chat_id = message.chat.id
-                with open(f'{message.chat.id}.txt', 'a+') as text_path:   
-                  for page in range (0,num_of_pages):              
-                      page_no = pdf_reader.getPage(page) # Iteration of page number
-                      page_content += page_no.extractText()
-                await txt.edit(f"Creating Your Audio Book...\n Please Don't Do Anything")
-                output_text = page_content + Thanks
-              # Change Voice by editing the Language
-                language = 'en-in'  # 'en': ['en-us', 'en-ca', 'en-uk', 'en-gb', 'en-au', 'en-gh', 'en-in',
-                                    # 'en-ie', 'en-nz', 'en-ng', 'en-ph', 'en-za', 'en-tz'],
-                tts_file = gTTS(text=output_text, lang=language, slow=False) 
-                tts_file.save(f"{message.chat.id}.mp3")      
-                with open(f"{message.chat.id}.mp3", "rb") as speech:
-                      await bot.send_voice(chat_id, speech)   
-                await txt.edit("𝚃𝙷𝙰𝙽𝙺𝚂 𝙵𝙾𝚁 𝚄𝚂𝙸𝙽𝙶 𝙼𝙴...☺️\n©𝙲𝙸𝙽𝙴𝙼𝙰𝙻𝙰.𝙲𝙾𝙼")    
-                os.remove(pdf_path)  
-                
-                
-           else :
-                await message.reply("Please Reply to PDF file")
- except Exception as error :
-           print(error)
-           await txt.delete()
-           os.remove(pdf_path)
+    try:
+        if message.reply_to_message and message.reply_to_message.document:
+            pdf_path = os.path.join(DOWNLOAD_LOCATION, f"{message.chat.id}.pdf")
+            txt = await message.reply("Downloading PDF...")
+            await message.reply_to_message.download(pdf_path)
+            await txt.edit("Downloaded PDF")
+
+            pdf = open(pdf_path, 'rb')
+            pdf_reader = PyPDF2.PdfFileReader(pdf)
+            num_of_pages = pdf_reader.getNumPages()
+            await txt.edit(f"Found {num_of_pages} page(s)")
+
+            page_content = ""
+            for page in range(num_of_pages):
+                page_no = pdf_reader.getPage(page)
+                page_content += page_no.extractText()
+
+            await txt.edit("Creating Your Audio Book... Please wait.")
+            output_text = page_content + Thanks
+            language = 'en-in'
+            tts_file = gTTS(text=output_text, lang=language, slow=False)
+            audio_path = os.path.join(DOWNLOAD_LOCATION, f"{message.chat.id}.mp3")
+            tts_file.save(audio_path)
+
+            with open(audio_path, "rb") as speech:
+                await bot.send_voice(message.chat.id, speech)
+
+            await txt.edit("Thanks for using this service!")
+            os.remove(pdf_path)
+            os.remove(audio_path)
+        else:
+            await message.reply("Please reply to a PDF file.")
+    except Exception as error:
+        print(error)
+        await txt.delete()
+        os.remove(pdf_path)
