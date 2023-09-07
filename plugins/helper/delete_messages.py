f37cfd5e0d58d37c8a0a41935c54c7bf14c2bb89
Sample First <132287607+SampleFirst@users.noreply.github.com>
2023-09-07 22:38:57 +0530

    Update delete_messages.py

--- plugins/helper/delete_messages.py
+++ plugins/helper/delete_messages.py
@@ -1,29 +1,29 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message
 
-
-# Define the command handler
-@Client.on_message(filters.command("delete_messages") & filters.private)
+# Define a command handler
+@Client.on_message(filters.command("delete_msg"))
 async def delete_all_messages(client, message: Message):
-    try:
-        # Get the chat ID from the command arguments
-        chat_id = message.command[1]
-
-        # Check if the chat_id is valid (you might want to add more validation)
-        if chat_id.isdigit():
+    # Check if the user is an administrator (or the bot itself) in the chat
+    chat_id = message.command[1] if len(message.command) > 1 else None
+    if chat_id:
+        try:
             chat_id = int(chat_id)
+        except ValueError:
+            await message.reply("Invalid chat ID. Please provide a valid numeric chat ID.")
+            return
 
-            # Get all messages in the chat
-            messages = await client.get_history(chat_id)
+        chat = await client.get_chat(chat_id)
+        if not chat:
+            await message.reply("Chat not found. Please provide a valid chat ID.")
+            return
 
-            # Delete each message in the chat
-            for msg in messages:
-                await client.delete_messages(chat_id, msg.message_id)
-
-            # Notify the user that messages have been deleted
-            await message.reply_text(f"All messages in chat {chat_id} have been deleted.")
+        # Check if the user has the necessary permissions to delete messages
+        if chat.type in ["private", "group", "supergroup"]:
+            # Delete all messages in the specified chat
+            await message.delete(revoke=True)
+            await message.reply(f"All messages in chat {chat.title} have been deleted.")
         else:
-            await message.reply_text("Invalid chat ID provided.")
-
-    except Exception as e:
-        await message.reply_text(f"An error occurred: {str(e)}")
+            await message.reply("This command can only be used in private chats, groups, or supergroups.")
+    else:
+        await message.reply("Please provide a valid chat ID with the command, like this: /delete_all <chat_id>")
