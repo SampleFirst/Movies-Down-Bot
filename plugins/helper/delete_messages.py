3648dfcf29a2311d915e6e2bc910429613c2ff85
Sample First <132287607+SampleFirst@users.noreply.github.com>
2023-09-08 00:03:44 +0530

    Update delete_messages.py

--- plugins/helper/delete_messages.py
+++ plugins/helper/delete_messages.py
@@ -1,14 +1,17 @@
-from pyrogram import filters, Client, enums
+from pyrogram import Client, filters, enums
 from pyrogram.types import Message
+from info import ADMINS
 
-@Client.on_message(filters.command("purge_all") & (filters.group | filters.channel))                   
+
+@Client.on_message(filters.command("purge") & (filters.group | filters.channel))
 async def purge(client, message: Message):
-    if message.chat.type not in (enums.ChatType.SUPERGROUP, enums.ChatType.CHANNEL):
+    if message.chat.type not in [enums.ChatType.SUPERGROUP, enums.ChatType.CHANNEL]:
         return
-    is_admin = await admin_check(message)
+    is_admin = message.from_user.id in ADMINS
     if not is_admin:
+        await message.reply_text("You are not authorized to use this command.", quote=True)
         return
-    status_message = await message.reply_text("...", quote=True)
+    status_message = await message.reply_text("Deleting messages...", quote=True)
     await message.delete()
     message_ids = []
     count_deletions = 0
@@ -16,7 +19,7 @@ async def purge(client, message: Message):
         for a_s_message_id in range(message.reply_to_message.message_id, message.message_id):
             message_ids.append(a_s_message_id)
             if len(message_ids) == 100:
-                await client.delete_messages(chat_id=message.chat.id, message_ids=message_ids, revoke=True)              
+                await client.delete_messages(chat_id=message.chat.id, message_ids=message_ids, revoke=True)
                 count_deletions += len(message_ids)
                 message_ids = []
         if len(message_ids) > 0:
