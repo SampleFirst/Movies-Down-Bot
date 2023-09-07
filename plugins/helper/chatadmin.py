6e339edfba74386d63cc7f107bad570f793ec9a6
Sample First <132287607+SampleFirst@users.noreply.github.com>
2023-09-08 00:13:53 +0530

    Update chatadmin.py

--- plugins/helper/chatadmin.py
+++ plugins/helper/chatadmin.py
@@ -1,22 +1,21 @@
 import asyncio
 from pyrogram import Client, filters
 from pyrogram.types import ChatMember
-from info import *
-
+from info import ADMINS
 
 # Define a command to list admins and owners
 @Client.on_message(filters.command("admins") & filters.user(ADMINS))
 async def list_admins(client, message):
     try:
         chat_id = message.chat.id
-        chat = await client.get_chat(chat_id)  # Get the chat object
-        async for member in chat.iter_chat_members():
-            if member.status in [ChatMember.ADMINISTRATOR, ChatMember.OWNER]:
+        # Use the client object to iterate over chat members
+        async for member in client.iter_chat_members(chat_id):
+            if member.status in ["administrator", "creator"]:
                 admin_info = {
                     'User ID': member.user.id,
                     'Username': member.user.username,
                     'Status': member.status,
-                    'Custom Title': member.custom_title,
+                    'Custom Title': member.custom_title if member.custom_title else "None",
                     'Can Be Edited': member.can_be_edited,
                     'Privileges': member.privileges,
                     'Permissions': member.permissions,
