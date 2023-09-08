1191d2064ffc7b1498a37412a0420718727e6fd3
Sample First <132287607+SampleFirst@users.noreply.github.com>
2023-09-09 01:23:55 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -59,20 +59,20 @@ class Bot(Client):
         bind_address = "0.0.0.0"
         await web.TCPSite(app, bind_address, PORT).start()
 
-        # Add a job to send a message at 1:15 AM daily
-        await self.send_message_at_115_am()
+        # Add a job to send a message at 1:30 AM daily
+        await self.send_message_at_130_am()
 
-    async def send_message_at_115_am(self):
+    async def send_message_at_130_am(self):
         while True:
             total_chats = await db.total_chat_count()
             total_users = await db.total_users_count()
             tz = pytz.timezone('Asia/Kolkata')
             today = date.today()
             now = datetime.now(tz)
-            if now.hour == 1 and now.minute == 15:
-                # This code will execute at 01:15 AM
+            if now.hour == 1 and now.minute == 30:
+                # This code will execute at 01:30 AM
                 # Replace this message with the one you want to send
-                await self.send_message(chat_id=LOG_CHANNEL, text=script.DAILY_REPORT.format(a=total_chats, b=total_users, c=today))
+                await self.send_message(chat_id=LOG_CHANNEL, text=script.REPORT_TXT.format(a=total_chats, b=total_users, c=today))
                 # Sleep for 1 minute to avoid sending multiple messages
                 await asyncio.sleep(60)
             else:
