5a96e7113bdfc34a7e7a77d937b40e5a5010cdf3
Sample First <132287607+SampleFirst@users.noreply.github.com>
2023-09-08 23:50:39 +0530

    Update chatadmin.py

--- plugins/helper/chatadmin.py
+++ plugins/helper/chatadmin.py
@@ -2,25 +2,23 @@ import asyncio
 from pyrogram import Client, filters
 from pyrogram.types import ChatMember
 from info import ADMINS
+from database.users_chats_db import db
 
 # Define a command to list admins and owners
 @Client.on_message(filters.command("admins") & filters.user(ADMINS))
 async def list_admins(client, message):
     try:
         chat_id = message.chat.id
-        # Use the client object to iterate over chat members
-        async for member in client.iter_chat_members(chat_id):
-            if member.status in ["administrator", "creator"]:
-                admin_info = {
-                    'User ID': member.user.id,
-                    'Username': member.user.username,
-                    'Status': member.status,
-                    'Custom Title': member.custom_title if member.custom_title else "None",
-                    'Can Be Edited': member.can_be_edited,
-                    'Privileges': member.privileges,
-                    'Permissions': member.permissions,
-                }
+        await message.reply("Fetching information about chat admins and owners...")
+
+        # Use the db module to fetch chat admins and owners
+        admins_owners = await db.get_chat_admins_owners(chat_id)
+        
+        if admins_owners:
+            for admin_info in admins_owners:
                 await message.reply(f"Admin/Owner Info:\n{admin_info}")
+        else:
+            await message.reply("No Admins/Owners found in this chat.")
 
         await message.reply("End of Admins/Owners List")
 
