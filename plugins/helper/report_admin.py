e089f2969b39e609040fbb7afadb19545f154980
Sample First <132287607+SampleFirst@users.noreply.github.com>
2023-09-09 02:26:52 +0530

    Update report_admin.py

--- plugins/helper/report_admin.py
+++ plugins/helper/report_admin.py
@@ -7,8 +7,7 @@ from info import ADMINS
 # Dictionary to store reported messages
 reported_messages = {}
 
-
-@Client.on_message((filters.command(["report"]) | filters.regex("@admins|@admin")) & filters.group)
+@Client.on_message(filters.command(["report"]) & filters.group)
 async def report_user(client, message):
     if not message.group:
         await message.reply_text("Reporting commands are only available in group chats.")
@@ -21,15 +20,15 @@ async def report_user(client, message):
         report = f"𝖱𝖾𝗉𝗈𝗋𝗍𝖾𝗋: {mention} ({reporter})" + "\n"
         report += f"𝖬𝖾𝗌𝗌𝖺𝗀𝖾: {message.reply_to_message.link}"
         
-        # Store reported message in a dictionary
-        reported_messages[message.message_id] = report
+        # Store reported message with a unique key (message_id)
+        reported_messages[message.reply_to_message.message_id] = report
 
-        # Using latest pyrogram's enums to filter out chat administrators
-        async for admin in bot.get_chat_members(chat_id=message.chat.id, filter=enums.ChatMembersFilter.ADMINISTRATORS):
-            if not admin.user.is_bot: # Filtering bots and prevent sending message to bots | Message will be send only to user admins
+        # Forward the report to chat administrators
+        async for admin in client.iter_chat_members(chat_id, filter=enums.ChatMembersFilter.ADMINISTRATORS):
+            if not admin.user.is_bot:
                 try:
                     await message.reply_to_message.forward(admin.user.id)
-                    await bot.send_message(
+                    await client.send_message(
                         chat_id=admin.user.id,
                         text=report,
                         disable_web_page_preview=True
@@ -40,10 +39,14 @@ async def report_user(client, message):
 @Client.on_message(filters.command(["getreport"]) & filters.private)
 async def get_report(client, message):
     if message.from_user.id in ADMINS:
-        if message.message_id in reported_messages:
-            report = reported_messages[message.message_id]
-            await message.reply_text(report)
+        if message.text.isdigit():
+            message_id = int(message.text)
+            if message_id in reported_messages:
+                report = reported_messages[message_id]
+                await message.reply_text(report)
+            else:
+                await message.reply_text("Report not found.")
         else:
-            await message.reply_text("Report not found or has expired.")
+            await message.reply_text("Invalid command format. Use /getreport <message_id>.")
     else:
         await message.reply_text("This command is only for ADMINS.")
