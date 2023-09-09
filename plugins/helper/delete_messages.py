88635e38337531d8e9218e89308661ca8728e5a8
Sample First <132287607+SampleFirst@users.noreply.github.com>
2023-09-09 15:28:27 +0530

    Update delete_messages.py

--- plugins/helper/delete_messages.py
+++ plugins/helper/delete_messages.py
@@ -3,20 +3,26 @@ from pyrogram import Client, filters
 from pyrogram.types import Message
 from info import ADMINS
 
-@Client.on_message(filters.command("purge") & (filters.group | filters.channel))
-async def purge(client, message):
-    # Check if the chat type is a supergroup or channel
-    if message.chat.type not in ('supergroup', 'channel'):
-        return
 
+@Client.on_message(filters.command("purge") & (filters.group | filters.supergroup | filters.channel))
+async def purge(client, message):
+    # Check if the chat type is a supergroup, group, or channel
+    chat_type = ""
+    if message.chat.type == 'group':
+        chat_type = "group"
+    elif message.chat.type == 'supergroup':
+        chat_type = "supergroup"
+    elif message.chat.type == 'channel':
+        chat_type = "channel"
+    
     # Check if the user sending the command is an admin
     is_admin = message.from_user.id in ADMINS
     if not is_admin:
-        await message.reply_text("You are not authorized to use this command.", quote=True)
+        await message.reply_text("You are not authorized to use this command in this {}.".format(chat_type), quote=True)
         return
 
     # Send a status message to indicate the purge is in progress
-    status_message = await message.reply_text("Purging...", quote=True)
+    status_message = await message.reply_text("Purging in {}...".format(chat_type), quote=True)
 
     count_deleted_messages = 0  # Initialize count_deleted_messages
     message_ids = []
@@ -47,7 +53,7 @@ async def purge(client, message):
             count_deleted_messages += len(message_ids)
 
     # Edit the status message to show the number of deleted messages
-    await status_message.edit_text(f"Deleted {count_deleted_messages} messages")
+    await status_message.edit_text(f"Deleted {count_deleted_messages} messages in {}.".format(count_deleted_messages, chat_type))
 
     # Wait for 5 seconds before deleting the status message
     await asyncio.sleep(5)
