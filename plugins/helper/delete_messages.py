82492c00bd1354a273f1ec04f372b25d5668285a
Sample First <132287607+SampleFirst@users.noreply.github.com>
2023-09-09 23:06:16 +0530

    Update delete_messages.py

--- plugins/helper/delete_messages.py
+++ plugins/helper/delete_messages.py
@@ -20,33 +20,18 @@ async def purge(client, message):
     status_message = await message.reply_text("Purging in {}...".format(chat_type), quote=True)
 
     count_deleted_messages = 0  # Initialize count_deleted_messages
-    message_ids = []
 
     if message.reply_to_message:
-        # Collect message IDs for deletion within the defined range
         try:
-            async for msg in client.iter_history_messages(
+            async for msg in client.get_history(
                 chat_id=message.chat.id,
-                reverse=True,
-                offset_id=message.reply_to_message.message_id,
+                offset_id=message.reply_to_message.message_id + 1,  # Add 1 to start after the replied message
             ):
-                message_ids.append(msg.message_id)
-                # Delete messages in batches of 100 to avoid rate limits
-                if len(message_ids) == 100:
-                    await client.delete_messages(
-                        chat_id=message.chat.id,
-                        message_ids=message_ids
-                    )
-                    count_deleted_messages += len(message_ids)
-                    message_ids = []
-    
-            # Delete any remaining messages
-            if len(message_ids) > 0:
                 await client.delete_messages(
                     chat_id=message.chat.id,
-                    message_ids=message_ids
+                    message_ids=msg.message_id
                 )
-                count_deleted_messages += len(message_ids)
+                count_deleted_messages += 1
         except Exception as e:
             await status_message.edit_text("An error occurred while purging messages: {}".format(str(e)))
             return
@@ -57,4 +42,3 @@ async def purge(client, message):
     # Wait for 5 seconds before deleting the status message
     await asyncio.sleep(5)
     await status_message.delete()
-    
