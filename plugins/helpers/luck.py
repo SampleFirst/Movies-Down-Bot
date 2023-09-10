5327659beaaa11010934b968249cb7a37fa5a82c
Sample First <132287607+SampleFirst@users.noreply.github.com>
2023-09-10 20:08:14 +0530

    Update luck.py

--- plugins/helpers/luck.py
+++ plugins/helpers/luck.py
@@ -1,22 +1,15 @@
-
-from pyrogram import Client, filters, enums
-from info import COMMAND_HAND_LER
-from plugins.helper_functions.cust_p_filters import f_onw_fliter
-
-# LUCK------------ https://telegram.me/Josprojects ------------ #
+from pyrogram import Client, filters
 
 # EMOJI CONSTANTS
 TRY_YOUR_LUCK = "🎰"
 # EMOJI CONSTANTS
 
-@Client.on_message(
-    filters.command(["luck", "cownd"])
-)
+@Client.on_message(filters.command(["luck", "cownd"]))
 async def luck_cownd(client, message):
-    """ /luck an @animatedluck """
-    rep_mesg_id = message.id
+    """ /luck command to try your luck """
+    rep_mesg_id = message.message_id
     if message.reply_to_message:
-        rep_mesg_id = message.reply_to_message.id
+        rep_mesg_id = message.reply_to_message.message_id
     await client.send_dice(
         chat_id=message.chat.id,
         emoji=TRY_YOUR_LUCK,
