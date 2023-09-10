3ed41cc0731e17534d15e3490edb2673ce7e3a99
Sample First <132287607+SampleFirst@users.noreply.github.com>
2023-09-10 20:04:06 +0530

    Update goal.py

--- plugins/helpers/goal.py
+++ plugins/helpers/goal.py
@@ -1,22 +1,16 @@
-
-from pyrogram import Client, filters, enums
-from info import COMMAND_HAND_LER
+from pyrogram import Client, filters
 from plugins.helper_functions.cust_p_filters import f_onw_fliter
 
 # EMOJI CONSTANTS
 GOAL_E_MOJI = "⚽"
 # EMOJI CONSTANTS
 
-
-@Client.on_message(
-    filters.command(["goal", "shoot"], COMMAND_HAND_LER) &
-    f_onw_fliter
-)
+@Client.on_message(filters.command(["goal", "shoot"]) & f_onw_fliter)
 async def roll_dice(client, message):
     """ @Goal """
-    rep_mesg_id = message.id
+    rep_mesg_id = message.message_id
     if message.reply_to_message:
-        rep_mesg_id = message.reply_to_message.id
+        rep_mesg_id = message.reply_to_message.message_id
     await client.send_dice(
         chat_id=message.chat.id,
         emoji=GOAL_E_MOJI,
