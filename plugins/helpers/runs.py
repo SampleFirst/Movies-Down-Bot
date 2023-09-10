dd4b65d288b653e558f65fed9bd5c6b348d0c68c
Sample First <132287607+SampleFirst@users.noreply.github.com>
2023-09-10 20:28:21 +0530

    Update runs.py

--- plugins/helpers/runs.py
+++ plugins/helpers/runs.py
@@ -1,47 +1,34 @@
 import random
-from pyrogram import Client, filters, enums
-from info import COMMAND_HAND_LER
-from plugins.helper_functions.cust_p_filters import f_onw_fliter
-
+from pyrogram import Client, filters
 
 RUN_STRINGS = (
-    "ഓ.. ധിക്കാരം... പഴേപോലെ തന്നെ....ഒരു മാറ്റോമില്ല.....ചുമ്മാതല്ല ഗതി പിടിക്കാത്തത്....!!!",
-    "അള്ളാ... പിള്ളേരുടെ ഓരോ... പെഷനെ...",
-    "എനിക്ക് എഴുതാൻ അല്ലെ അറിയൂ സാറേ.... വായിക്കാൻ അറിയില്ലല്ലോ....",
-    "ഇന്ന് ഇനി നീ മിണ്ടരുത്... ഇന്നത്തെ കോട്ട കഴിഞ്ഞ്.....",
-    "ചാരമാണെന്ന് കരുതി ചെകയാൻ നിൽക്കണ്ട കനൽ കെട്ടിട്ടില്ലെങ്കിൽ പൊള്ളും.",
-    "ഒറ്റ ജീവിതമേ ഉള്ളു മനസിലാക്കിക്കോ, സ്വർഗ്ഗമില്ല നരകമില്ല, 'ഒറ്റ ജീവിതം', അത് എവിടെ എങ്ങനെ വേണമെന്ന് അവനവൻ തീരുമാനിക്കും",
-    "വാട്ട് എ ബോംബെസ്റ്റിക് എക്സ്പ്ലോഷൻ! സച് എ ടെറിഫിക് ഡിസ്ക്ലോസ്!!",
-    "ഗോ എവേ സ്ടുപ്പിഡ് ഇൻ ദി ഹൗസ് ഓഫ് മൈ വൈഫ്‌ ആൻഡ് ഡോട്ടർ യൂവിൽ നോട്ട് സി എനി മിനിറ്റ് ഓഫ് ദി ടുഡേ... ഇറങ്ങി പോടാ..",
-    "ഐ കാൻ ഡു ദാറ്റ്‌ ഡു കാൻ ഐ ദാറ്റ്‌",
-    "ക്രീം ബിസ്കറ്റിൽ ക്രീം ഉണ്ടന്ന് കരുതി ടൈഗർ ബിസ്കറ്റിൽ ടൈഗർ ഉണ്ടാകണമെന്നില്ല. പണി പാളും മോനെ...",
-    "പട പേടിച്ചു പന്തളത്തു ചെന്നപ്പോ പന്തോം കുത്തി പട പന്തളത്തോട്ടെന്ന് പറഞ്ഞ പോലെ ആയല്ലോ.",
-    "എന്റ കർത്താവെ.... എന്നെ നീ നല്ലവനാകാൻ സമ്മതിക്കൂല്ല അല്ലെ.",
-    "കാർ എൻജിൻ ഔട്ട് കംപ്ലീറ്റ്‌ലി......",
-    "തള്ളെ കലിപ്പ് തീരണില്ലല്ലോ!!",
-    "പാതിരാത്രിക്ക് നിന്റെ അച്ഛൻ ഉണ്ടാക്കി വെച്ചിരിക്കുന്നോ പൊറോട്ടയും ചിക്കനും....",
-    "ഓ പിന്നെ നീ ഒക്കെ പ്രേമിക്കുമ്പോൾ അത് പ്രണയം.... നമ്മൾ ഒക്കെ പ്രേമിക്കുമ്പോൾ അത് കമ്പി....",
-    "ദൈവമേ എന്നെ മാത്രം രക്ഷിക്കണേ....",
-    "അവളെ ഓർത്ത് കുടിച്ച കള്ളും നനഞ്ഞ മഴയും വേസ്റ്റ്....",
-    "ഇത്രേം കാലം എവിടെ ആയിരുന്നു....!",
-    "ഇൻഗ്ലീഷ് തീരെ പിടി ഇല്ല അല്ലെ....",
-    "ആൾ ദി ഡ്രീംസ്‌ ലൈക്‌ ട്വിങ്കിൽ സ്റ്റാർസ്...",
-    "എന്റെ പ്രാന്തൻ മുത്തപ്പാ അവനെ ഒരു വഴിയാക്കി തരണേ",
-    "പെങ്ങളെ കെട്ടിയ സ്ത്രീധന തുക തരുമോ അളിയാ",
-    "നീ വല്ലാതെ ക്ഷീണിച്ചു പൊയി",
-    "കണ്ണിലെണ്ണയൊഴിച്ചു കാത്തിരിക്കുവായിരുന്നളിയാ.",
-    "ചെല്ലാക്കണ്ടു എന്നിച്ചു പോടാ തടി.യാ .\
-    ഷട്ട് ഉഒ യുവർ മൗത് ബ്ലഡി gramavasis.",
-    "പോയി ചാവട .\
-    നിന്നെ കൊണ്ട് ചാവാൻ patto.",
-    "നിന്നെ കൊണ്ട് നാട്ടുകാർക്കും ഗുണോല്ല്യ വിട്ടുകാർക്കും ഗുണോല്ല്യ എന്തിനാ ഇങ്ങനെ നാണം കേട്ടു ജീവിക്കുന്നട പാട് വാഴെ ചെങ്കതളി വാഴ .", 
+    "Oh... destiny... just the same... no change... can't escape fate...!!!",
+    "Hey... every person's passion...",
+    "I don't know how to write, dear. I don't even know how to read...",
+    "Today, you won't escape... after winning today's race...",
+    "If there's no spark to check what you believe is a lie, it'll burst.",
+    "Only one life, make up your mind, there's no heaven, there's no hell, it's just one life, how you want it, it's up to you.",
+    "Go Avengers! Suck it Disclosure!",
+    "Go, stupid in the House of My Wife and Daughter, Yuvi! Not in a minute off the Today... get out.",
+    "I can do that, I can't do that.",
+    "In biscuits, there's cream, but thinking there should be a tiger in Tiger biscuits, it's absurd. Work is a pain, dude...",
+    "Threw the book, hit the wall, didn't it work!",
+    "My dear Lord... You are the only one who can save me...",
+    "She gave me a chicken with cream biscuits...",
+    "Where was all this time!",
+    "English, not a single word...",
+    "A guy's dreams are like twinkling stars...",
+    "My dear Grandfather, you made him a way.",
+    "Take away the dowry you got from the guys, dear.",
+    "You wandered aimlessly without achieving anything.",
+    "Apply kohl to your eyes, it seems.",
+    "Go away, you devil. You people, die.",
+    "Go and die. Why did you leave me?",
+    "Why did you leave? What do people say?",
 )
 
 
-@Client.on_message(
-    filters.command("runs", COMMAND_HAND_LER) &
-    f_onw_fliter
-)
+@Client.on_message(filters.command("runs"))
 async def runs(_, message):
     """ /runs strings """
     effective_string = random.choice(RUN_STRINGS)
