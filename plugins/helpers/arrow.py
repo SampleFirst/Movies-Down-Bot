e5a59b0c22c04a1c6330fe06d2d56776de620eb9
Sample First <132287607+SampleFirst@users.noreply.github.com>
2023-09-10 19:53:20 +0530

    Update arrow.py

--- plugins/helpers/arrow.py
+++ plugins/helpers/arrow.py
@@ -1,19 +1,11 @@
-# codes added by @lallu_tg
-# use with proper credits
-
-from pyrogram import Client, filters, enums
-from info import COMMAND_HAND_LER
+from pyrogram import Client, filters
 from plugins.helper_functions.cust_p_filters import f_onw_fliter
 
 # EMOJI CONSTANTS
 DART_E_MOJI = "🎯"
 # EMOJI CONSTANTS
 
-
-@Client.on_message(
-    filters.command(["throw", "dart"], COMMAND_HAND_LER) &
-    f_onw_fliter
-)
+@Client.on_message(filters.command(["throw", "dart"]) & f_onw_fliter)
 async def throw_dart(client, message):
     """ /throw an @AnimatedDart """
     rep_mesg_id = message.id
