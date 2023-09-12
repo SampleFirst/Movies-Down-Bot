ffa0e0c74059568e6ef559fc7df9baa79398b734
Sample First <132287607+SampleFirst@users.noreply.github.com>
2023-09-12 22:39:49 +0530

    Update gfilters_mdb.py

--- database/gfilters_mdb.py
+++ database/gfilters_mdb.py
@@ -2,74 +2,68 @@ import pymongo
 from info import DATABASE_URI, DATABASE_NAME
 from pyrogram import enums
 import logging
+
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.ERROR)
 
 myclient = pymongo.MongoClient(DATABASE_URI)
 mydb = myclient[DATABASE_NAME]
 
-
-
 async def add_gfilter(gfilters, text, reply_text, btn, file, alert):
     mycol = mydb[str(gfilters)]
-    # mycol.create_index([('text', 'text')])
 
     data = {
-        'text':str(text),
-        'reply':str(reply_text),
-        'btn':str(btn),
-        'file':str(file),
-        'alert':str(alert)
+        'text': str(text),
+        'reply': str(reply_text),
+        'btn': str(btn),
+        'file': str(file),
+        'alert': str(alert)
     }
 
     try:
-        mycol.update_one({'text': str(text)},  {"$set": data}, upsert=True)
-    except:
-        logger.exception('Some error occured!', exc_info=True)
-             
-     
+        mycol.update_one({'text': str(text)}, {"$set": data}, upsert=True)
+    except Exception as e:
+        logger.exception('Some error occurred!', exc_info=True)
+
 async def find_gfilter(gfilters, name):
     mycol = mydb[str(gfilters)]
-    
-    query = mycol.find( {"text":name})
-    # query = mycol.find( { "$text": {"$search": name}})
+
+    query = mycol.find({"text": name})
+
     try:
         for file in query:
-            reply_text = file['reply']
-            btn = file['btn']
-            fileid = file['file']
-            try:
-                alert = file['alert']
-            except:
-                alert = None
+            reply_text = file.get('reply', '')
+            btn = file.get('btn', '')
+            fileid = file.get('file', '')
+            alert = file.get('alert', None)
         return reply_text, btn, alert, fileid
-    except:
+    except Exception as e:
         return None, None, None, None
-
 
 async def get_gfilters(gfilters):
     mycol = mydb[str(gfilters)]
 
     texts = []
     query = mycol.find()
+
     try:
         for file in query:
-            text = file['text']
+            text = file.get('text', '')
             texts.append(text)
-    except:
+    except Exception as e:
         pass
     return texts
 
-
 async def delete_gfilter(message, text, gfilters):
     mycol = mydb[str(gfilters)]
-    
-    myquery = {'text':text }
+
+    myquery = {'text': text}
     query = mycol.count_documents(myquery)
+
     if query == 1:
         mycol.delete_one(myquery)
         await message.reply_text(
-            f"'`{text}`'  deleted. I'll not respond to that gfilter anymore.",
+            f"'`{text}`' deleted. I'll not respond to that gfilter anymore.",
             quote=True,
             parse_mode=enums.ParseMode.MARKDOWN
         )
@@ -78,23 +72,22 @@ async def delete_gfilter(message, text, gfilters):
 
 async def del_allg(message, gfilters):
     if str(gfilters) not in mydb.list_collection_names():
-        await message.edit_text("Nothin!")
+        await message.edit_text("Nothing to remove!")
         return
 
     mycol = mydb[str(gfilters)]
+
     try:
         mycol.drop()
-        await message.edit_text(f"All filters has been removed")
-    except:
+        await message.edit_text(f"All filters have been removed")
+    except Exception as e:
         await message.edit_text("Couldn't remove all filters!")
-        return
 
 async def count_gfilters(gfilters):
     mycol = mydb[str(gfilters)]
 
     count = mycol.count()
     return False if count == 0 else count
-
 
 async def gfilter_stats():
     collections = mydb.list_collection_names()
@@ -111,3 +104,4 @@ async def gfilter_stats():
     totalcollections = len(collections)
 
     return totalcollections, totalcount
+    
