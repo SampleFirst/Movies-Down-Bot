9ecc81e9ce22b04a2ed35f86aad8254b09f97405
Sample First <132287607+SampleFirst@users.noreply.github.com>
2023-09-12 22:37:35 +0530

    Update filters_mdb.py

--- database/filters_mdb.py
+++ database/filters_mdb.py
@@ -2,80 +2,73 @@ import pymongo
 from pyrogram import enums
 from info import DATABASE_URI, DATABASE_NAME
 import logging
+
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.ERROR)
 
 myclient = pymongo.MongoClient(DATABASE_URI)
 mydb = myclient[DATABASE_NAME]
 
-
-
 async def add_filter(grp_id, text, reply_text, btn, file, alert):
     mycol = mydb[str(grp_id)]
-    # mycol.create_index([('text', 'text')])
 
     data = {
-        'text':str(text),
-        'reply':str(reply_text),
-        'btn':str(btn),
-        'file':str(file),
-        'alert':str(alert)
+        'text': str(text),
+        'reply': str(reply_text),
+        'btn': str(btn),
+        'file': str(file),
+        'alert': str(alert)
     }
 
     try:
-        mycol.update_one({'text': str(text)},  {"$set": data}, upsert=True)
-    except:
-        logger.exception('Some error occured!', exc_info=True)
-             
-     
+        mycol.update_one({'text': str(text)}, {"$set": data}, upsert=True)
+    except Exception as e:
+        logger.exception('Some error occurred!', exc_info=True)
+
 async def find_filter(group_id, name):
     mycol = mydb[str(group_id)]
-    
-    query = mycol.find( {"text":name})
-    # query = mycol.find( { "$text": {"$search": name}})
+
+    query = mycol.find({"text": name})
+
     try:
         for file in query:
-            reply_text = file['reply']
-            btn = file['btn']
-            fileid = file['file']
-            try:
-                alert = file['alert']
-            except:
-                alert = None
+            reply_text = file.get('reply', '')
+            btn = file.get('btn', '')
+            fileid = file.get('file', '')
+            alert = file.get('alert', None)
         return reply_text, btn, alert, fileid
-    except:
+    except Exception as e:
         return None, None, None, None
-
 
 async def get_filters(group_id):
     mycol = mydb[str(group_id)]
 
     texts = []
     query = mycol.find()
+
     try:
         for file in query:
-            text = file['text']
+            text = file.get('text', '')
             texts.append(text)
-    except:
+    except Exception as e:
         pass
     return texts
 
-
 async def delete_filter(message, text, group_id):
     mycol = mydb[str(group_id)]
-    
-    myquery = {'text':text }
+
+    myquery = {'text': text}
     query = mycol.count_documents(myquery)
+
     if query == 1:
         mycol.delete_one(myquery)
         await message.reply_text(
-            f"'`{text}`'  deleted. I'll not respond to that filter anymore.",
+            f"'`{text}`' deleted. I'll not respond to that filter anymore.",
             quote=True,
             parse_mode=enums.ParseMode.MARKDOWN
         )
     else:
         await message.reply_text("Couldn't find that filter!", quote=True)
-
 
 async def del_all(message, group_id, title):
     if str(group_id) not in mydb.list_collection_names():
@@ -83,20 +76,18 @@ async def del_all(message, group_id, title):
         return
 
     mycol = mydb[str(group_id)]
+
     try:
         mycol.drop()
-        await message.edit_text(f"All filters from {title} has been removed")
-    except:
-        await message.edit_text("Couldn't remove all filters from group!")
-        return
-
+        await message.edit_text(f"All filters from {title} have been removed")
+    except Exception as e:
+        await message.edit_text("Couldn't remove all filters from the group!")
 
 async def count_filters(group_id):
     mycol = mydb[str(group_id)]
 
     count = mycol.count()
     return False if count == 0 else count
-
 
 async def filter_stats():
     collections = mydb.list_collection_names()
@@ -113,3 +104,4 @@ async def filter_stats():
     totalcollections = len(collections)
 
     return totalcollections, totalcount
+    
