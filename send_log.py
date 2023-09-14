2a517fd8f74204731a1735ea4e647c1a608676eb
Sample First <132287607+SampleFirst@users.noreply.github.com>
2023-09-14 17:47:17 +0530

    Update send_log.py

--- send_log.py
+++ send_log.py
@@ -1,48 +1,78 @@
 import asyncio
-from datetime import datetime, date, timedelta
+from datetime import datetime, date
 import pytz
 from database.users_chats_db import db
 from info import LOG_CHANNEL
-from Script import script  
+from Script import script
+from utils import temp
 
-async def send_log(bot, interval):  # Pass 'bot' as the first argument
+async def send_log(bot, interval):
     while True:
-        total_chats = await db.total_chat_count()
-        total_users = await db.total_users_count()
         tz = pytz.timezone('Asia/Kolkata')
         now = datetime.now(tz)
 
-        if interval == 'daily' and now.hour == 16 and now.minute == 5:
-            # This code will execute at 11:50 PM for daily log
+        if interval == 'daily' and now.hour == 23 and now.minute == 59:
             today = date.today()
+            total_users = await db.total_users_count()
+            daily_users = await db.daily_users_count(today) + 1
+            total_chats = await db.total_chat_count()
+            daily_chats = await db.daily_chats_count(today) + 1
             time = now.strftime("%H:%M:%S %p")
-            await bot.send_message(chat_id=LOG_CHANNEL, text=script.REPORT_TXT.format(a=total_chats, b=total_users, c=today, d=time))
-            # Calculate time until the next day at 11:50 PM
-            next_execution = datetime(now.year, now.month, now.day, 23, 50) + timedelta(days=1)
-            sleep_time = (next_execution - now).total_seconds()
-            await asyncio.sleep(sleep_time)
+            day_name = today.strftime("%A")
 
-        elif interval == 'weekly' and now.weekday() == 6 and now.hour == 23 and now.minute == 59:
-            # This code will execute at 11:59 PM on Sundays for weekly log
-            today = date.today()
-            time = now.strftime("%H:%M:%S %p")
-            await bot.send_message(chat_id=LOG_CHANNEL, text=script.REPORT_TXT.format(a=total_chats, b=total_users, c=today, d=time))
-            # Calculate time until the next Sunday at 11:59 PM
-            days_until_next_sunday = 6 - now.weekday() + 7  # 6 - weekday + 7 days to next Sunday
-            next_execution = datetime(now.year, now.month, now.day, 23, 59) + timedelta(days=days_until_next_sunday)
-            sleep_time = (next_execution - now).total_seconds()
-            await asyncio.sleep(sleep_time)
+            await bot.send_message(chat_id=LOG_CHANNEL, text=script.REPORT_DAY_TXT.format(
+                a=temp.U_NAME, 
+                b=today, 
+                c=day_name, 
+                d=time, 
+                e=total_users, 
+                g=daily_users, 
+                h=total_chats, 
+                i=daily_chats))
+            await asyncio.sleep(300)
 
-        elif interval == 'monthly' and now.day == 1 and now.hour == 23 and now.minute == 59:
-            # This code will execute at 11:59 PM on the 1st day of the month for monthly log
+        elif interval == 'weekly' and now.weekday() == 6 and now.hour == 0 and now.minute == 0:
             today = date.today()
+            total_users = await db.total_users_count()
+            daily_users = await db.daily_users_count(today) + 1
+            total_chats = await db.total_chat_count()
+            daily_chats = await db.daily_chats_count(today) + 1
             time = now.strftime("%H:%M:%S %p")
-            await bot.send_message(chat_id=LOG_CHANNEL, text=script.REPORT_TXT.format(a=total_chats, b=total_users, c=today, d=time))
-            # Calculate time until the next month on the 1st at 11:59 PM
-            next_execution = datetime(now.year, now.month + 1, 1, 23, 59)
-            sleep_time = (next_execution - now).total_seconds()
-            await asyncio.sleep(sleep_time)
+            day_name = today.strftime("%A")
+
+            await bot.send_message(chat_id=LOG_CHANNEL, text=script.REPORT_WEEK_TXT.format(
+                a=temp.U_NAME, 
+                b=today, 
+                c=day_name, 
+                d=time, 
+                e=total_users, 
+                g=daily_users, 
+                h=total_chats, 
+                i=daily_chats))
+            await asyncio.sleep(300)
+
+        elif interval == 'monthly' and now.day == 1 and now.hour == 0 and now.minute == 1:
+            today = date.today()
+            total_users = await db.total_users_count()
+            daily_users = await db.daily_users_count(today) + 1
+            total_chats = await db.total_chat_count()
+            daily_chats = await db.daily_chats_count(today) + 1
+            time = now.strftime("%H:%M:%S %p")
+            month = now.strftime("%B")
+            day_name = today.strftime("%A")
+
+            await bot.send_message(chat_id=LOG_CHANNEL, text=script.REPORT_MONTH_TXT.format(
+                a=temp.U_NAME, 
+                b=today, 
+                c=day_name, 
+                d=month, 
+                e=time, 
+                g=total_users, 
+                h=daily_users, 
+                i=total_chats, 
+                j=daily_chats))
+            await asyncio.sleep(300)
 
         else:
-            # Sleep for 5 minute and check again
             await asyncio.sleep(300)
+            
