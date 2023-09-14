fd9c11cbeb1f1b0ac0a9e8fe36dbb4cd0258ff9d
Sample First <132287607+SampleFirst@users.noreply.github.com>
2023-09-15 02:32:40 +0530

    Update addadmin.py

--- plugins/helpers/addadmin.py
+++ plugins/helpers/addadmin.py
@@ -5,7 +5,7 @@ from info import ADMINS
 
 
 # Define your command handler for adding admin in a group
-@Client.on_message(filters.command("addgroupadmin") & filters.group)
+@Client.on_message(filters.command("addgroupadmin") & filters.group | filters.supergroup)
 async def add_group_admin(client, message):
     if message.from_user.id not in ADMINS:
         await message.reply("You must be an admin to use this command.")
@@ -17,35 +17,28 @@ async def add_group_admin(client, message):
 
     user_id = int(message.command[1])
 
-    try:
-        chat_info = await client.get_chat(message.chat.id)
-        is_supergroup = chat_info.is_verified
-
-        if is_supergroup:
-            privileges = ChatPermissions(
-                can_change_info=True,
-                can_delete_messages=True,
-                can_manage_chat=True,
-                can_restrict_members=True,
-                can_invite_users=True,
-                can_pin_messages=True,
-                can_promote_members=True
-            )
-            await message.reply(f"User added as an admin in supergroup {message.chat.id} with specified privileges.")
-        else:
-            privileges = ChatPermissions(
-                can_change_info=True,
-                can_delete_messages=True,
-                can_manage_chat=True,
-                can_restrict_members=True,
-                can_invite_users=True,
-                can_pin_messages=True,
-                can_promote_members=True
-            )
-            await message.reply(f"User added as an admin in group {message.chat.id} with specified privileges.")
-
-        await client.promote_chat_member(message.chat.id, user_id, permissions=privileges)
-    except UserNotParticipant:
-        await message.reply(f"The user must be a member of the chat to use this command.")
-    except Exception as e:
-        await message.reply(f"An error occurred: {str(e)}")
+    for chat_id in chat_group_ids:
+        try:
+            chat_info = await client.get_chat(chat_id)
+            chat_type = chat_info.type
+            
+            if chat_type in [enums.ChatType.GROUP, enums.ChatType.SUPERGROUP]:
+                privileges = ChatPrivileges(
+                    can_change_info=True,
+                    can_delete_messages=True,
+                    can_manage_video_chats=True,
+                    can_restrict_members=True,
+                    can_promote_members=True,
+                    can_invite_users=True,
+                    can_pin_messages=True,
+                    is_anonymous=True,
+                    can_manage_chat=True
+                )
+                await message.reply(f"User added as an admin in {chat_type} {chat_id} with specified privileges.")
+                await client.promote_chat_member(chat_id, user_id, privileges=privileges)
+            else:
+                await message.reply(f"This command can only be used in groups or supergroups.")
+        except UserNotParticipant:
+            await message.reply(f"The user must be a member of the chat {chat_id} to use this command.")
+        except Exception as e:
+            await message.reply(f"An error occurred: {str(e)}")
