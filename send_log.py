3916626bc2c67e9e5172f4c71be23812412da901
Sample First <132287607+SampleFirst@users.noreply.github.com>
2023-09-14 15:33:09 +0530

    Update send_log.py

--- send_log.py
+++ send_log.py
@@ -3,35 +3,34 @@ from datetime import datetime, date
 import pytz
 from database.users_chats_db import db
 from info import LOG_CHANNEL
-from Script import script 
+from Script import Script  
 
-
-async def send_log(self, interval):
+async def send_log(bot, interval):  # Pass 'bot' as the first argument
     while True:
         total_chats = await db.total_chat_count()
         total_users = await db.total_users_count()
         tz = pytz.timezone('Asia/Kolkata')
         now = datetime.now(tz)
         
-        if interval == 'daily' and now.hour == 15 and now.minute == 22:
+        if interval == 'daily' and now.hour == 23 and now.minute == 59:
             # This code will execute at 11:59 PM for daily log
             today = date.today()
             time = now.strftime("%H:%M:%S %p")
-            await self.send_message(chat_id=LOG_CHANNEL, text=script.REPORT_TXT.format(a=total_chats, b=total_users, c=today, d=time))
+            await bot.send_message(chat_id=LOG_CHANNEL, text=Script.REPORT_TXT.format(a=total_chats, b=total_users, c=today, d=time))
             await asyncio.sleep(60)  # Sleep for 1 minute to avoid sending multiple messages
 
         elif interval == 'weekly' and now.weekday() == 6 and now.hour == 23 and now.minute == 59:
             # This code will execute at 11:59 PM on Sundays for weekly log
             today = date.today()
             time = now.strftime("%H:%M:%S %p")
-            await self.send_message(chat_id=LOG_CHANNEL, text=script.REPORT_TXT.format(a=total_chats, b=total_users, c=today, d=time))
+            await bot.send_message(chat_id=LOG_CHANNEL, text=Script.REPORT_TXT.format(a=total_chats, b=total_users, c=today, d=time))
             await asyncio.sleep(60)  # Sleep for 1 minute to avoid sending multiple messages
 
         elif interval == 'monthly' and now.day == 1 and now.hour == 23 and now.minute == 59:
             # This code will execute at 11:59 PM on the 1st day of the month for monthly log
             today = date.today()
             time = now.strftime("%H:%M:%S %p")
-            await self.send_message(chat_id=LOG_CHANNEL, text=script.REPORT_TXT.format(a=total_chats, b=total_users, c=today, d=time))
+            await bot.send_message(chat_id=LOG_CHANNEL, text=Script.REPORT_TXT.format(a=total_chats, b=total_users, c=today, d=time))
             await asyncio.sleep(60)  # Sleep for 1 minute to avoid sending multiple messages
 
         else:
