90701d0259146b91125d42b502a8999a09f91b8b
Sample First <132287607+SampleFirst@users.noreply.github.com>
2023-09-16 20:51:14 +0530

    Update carbon.py

--- plugins/helper/carbon.py
+++ plugins/helper/carbon.py
@@ -1,50 +1,37 @@
-from pyrogram import filters
+from pyrogram import Client, filters
+from pyrogram.types import *
 from aiohttp import ClientSession
-from pyrogram import Client as bot
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-from asyncio import gather
-from datetime import datetime, timedelta
+from telegraph import upload_file
 from io import BytesIO
-from math import atan2, cos, radians, sin, sqrt
-from os import execvp
-from random import randint
-from re import findall
-from re import sub as re_sub
-from sys import executable
-import aiofiles
-import speedtest
-from PIL import Image
-from pyrogram.types import Message
-from info import S_GROUP
 
-aiohttpsession = ClientSession()
+ai_client = ClientSession()
 
-async def make_carbon(code):
-    url = "https://carbonara.vercel.app/api/cook"
-    async with aiohttpsession.post(url, json={"code": code}) as resp:
+async def make_carbon(code, tele=False):
+    url = "https://carbonara.solopov.dev/api/cook"
+    async with ai_client.post(url, json={"code": code}) as resp:
         image = BytesIO(await resp.read())
     image.name = "carbon.png"
+    if tele:
+        uf = upload_file(image)
+        image.close()
+        return f"https://graph.org{uf[0]}"
     return image
 
 
-@bot.on_message(filters.command("carbon"))
-async def carbon_func(_, message):
+@Client.on_message(filters.command("carbon"))
+async def carbon_func(b, message):
     if not message.reply_to_message:
-        return await message.reply_text(
-            "ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴛᴇxᴛ ᴍᴇssᴀɢᴇ ᴛᴏ ᴍᴀᴋᴇ ᴄᴀʀʙᴏɴ."
-        )
+        return await message.reply_text("ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴛᴇxᴛ ᴍᴇssᴀɢᴇ ᴛᴏ ᴍᴀᴋᴇ ᴄᴀʀʙᴏɴ.")
     if not message.reply_to_message.text:
-        return await message.reply_text(
-            "ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴛᴇxᴛ ᴍᴇssᴀɢᴇ ᴛᴏ ᴍᴀᴋᴇ ᴄᴀʀʙᴏɴ."
-        )
+        return await message.reply_text("ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴛᴇxᴛ ᴍᴇssᴀɢᴇ ᴛᴏ ᴍᴀᴋᴇ ᴄᴀʀʙᴏɴ.")
     user_id = message.from_user.id
     m = await message.reply_text("ᴘʀᴏᴄᴇssɪɴɢ...")
     carbon = await make_carbon(message.reply_to_message.text)
     await m.edit("ᴜᴘʟᴏᴀᴅɪɴɢ..")
     await message.reply_photo(
         photo=carbon,
-        caption="**ᴛʜɪs ᴘɪᴄ ɪs ɴɪᴄᴇ ᴏɴᴇ\nᴍᴀᴅᴇ ʙʏ ᴄɪɴᴇᴍᴀʟᴀ.ᴄᴏᴍ**",
-        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("sᴜᴘᴘᴏʀᴛ", url=S_GROUP)]]),                   
+        caption="**ᴍᴀᴅᴇ ʙʏ: @mkn_bots_updates**",
+        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("ꜱᴜᴩᴩᴏʀᴛ ᴜꜱ", url="https://t.me/mkn_bots_updates")]]),                   
     )
     await m.delete()
     carbon.close()
