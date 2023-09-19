c426fa2b7f87716917cc0055d20ece8f921947c7
Sample First <132287607+SampleFirst@users.noreply.github.com>
2023-09-19 11:52:56 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -548,47 +548,47 @@ async def cb_handler(client: Client, query: CallbackQuery):
             alert = alert.replace("\\n", "\n").replace("\\t", "\t")
             await query.answer(alert, show_alert=True)
                 
-if query.data.startswith("pmfile"):
-    try:
-        ident, file_id = query.data.split("#")
-        files_ = await get_file_details(file_id)
-        
-        if not files_:
-            await query.answer('No such file exists.')
-            return
-
-        files = files_[0]
-        title = files.file_name
-        size = get_size(files.file_size)
-        f_caption = files.caption
-
-        if CUSTOM_FILE_CAPTION:
-            try:
-                f_caption = CUSTOM_FILE_CAPTION.format(
-                    mention=query.from_user.mention,
-                    file_name='' if title is None else title,
-                    file_size='' if size is None else size,
-                    file_caption='' if f_caption is None else f_caption
-                )
-            except Exception as e:
-                logger.exception(e)
-
-        if f_caption is None:
-            f_caption = f"{files.file_name}"
-
-        if AUTH_CHANNEL and not await is_subscribed(client, query):
-            await query.answer(url=f"https://t.me/{temp.U_NAME}?start={ident}_{file_id}")
-        else:
-            await client.send_cached_media(
-                chat_id=query.from_user.id,
-                file_id=file_id,
-                caption=f_caption,
-                protect_content=True if ident == "pmfilep" else False
-            )
-            await query.answer('Process Complete...')
+    if query.data.startswith("pmfile"):
+        try:
+            ident, file_id = query.data.split("#")
+            files_ = await get_file_details(file_id)
+            
+            if not files_:
+                await query.answer('No such file exists.')
+                return
     
-    except Exception as e:
-        await query.answer(f"⚠️ Error {e}", show_alert=True)
+            files = files_[0]
+            title = files.file_name
+            size = get_size(files.file_size)
+            f_caption = files.caption
+    
+            if CUSTOM_FILE_CAPTION:
+                try:
+                    f_caption = CUSTOM_FILE_CAPTION.format(
+                        mention=query.from_user.mention,
+                        file_name='' if title is None else title,
+                        file_size='' if size is None else size,
+                        file_caption='' if f_caption is None else f_caption
+                    )
+                except Exception as e:
+                    logger.exception(e)
+    
+            if f_caption is None:
+                f_caption = f"{files.file_name}"
+    
+            if AUTH_CHANNEL and not await is_subscribed(client, query):
+                await query.answer(url=f"https://t.me/{temp.U_NAME}?start={ident}_{file_id}")
+            else:
+                await client.send_cached_media(
+                    chat_id=query.from_user.id,
+                    file_id=file_id,
+                    caption=f_caption,
+                    protect_content=True if ident == "pmfilep" else False
+                )
+                await query.answer('Process Complete...')
+        
+        except Exception as e:
+            await query.answer(f"⚠️ Error {e}", show_alert=True)
         
     if query.data.startswith("file"):
         ident, file_id = query.data.split("#")
