aab8a49be48bc457e518590c783312eb5701b7db
Sample First <132287607+SampleFirst@users.noreply.github.com>
2023-09-19 21:24:11 +0530

    Update premium.py

--- plugins/premium.py
+++ plugins/premium.py
@@ -1,16 +1,15 @@
 from pyrogram import Client, filters
 from datetime import datetime, timedelta
-from database.users_chats_db.py import db
+from database.users_chats_db import db
 
-
-@Client.on_message(filters.command("addpremium") filters.user(ADMINS))
+@Client.on_message(filters.command("addpremium") & filters.user(ADMINS))
 async def add_premium_user(bot, message):
     try:
         # Parse the user ID from the command arguments
         user_id = int(message.command[1])
         
         # Check if the user already exists in the database as a premium user
-        if await db.is_premium_user_exist(user_id):
+        if await db.check_premium_status(user_id):
             await message.reply("User is already a premium user.")
         else:
             # Calculate the start and end dates for the premium subscription (1 month duration)
@@ -25,3 +24,20 @@ async def add_premium_user(bot, message):
     
     except (IndexError, ValueError):
         await message.reply("Invalid command usage. Use /addpremium <user_id>")
+
+@Client.on_message(filters.command("checkpremium"))
+async def check_premium_status(bot, message):
+    try:
+        # Parse the user ID from the command arguments
+        user_id = int(message.command[1])
+
+        # Check the premium status of the user
+        is_premium = await db.check_premium_status(user_id)
+        
+        if is_premium:
+            await message.reply("User is a premium user.")
+        else:
+            await message.reply("User is not a premium user.")
+    
+    except (IndexError, ValueError):
+        await message.reply("Invalid command usage. Use /checkpremium <user_id>")
