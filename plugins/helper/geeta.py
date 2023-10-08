15a50f675ca0e402894a89070e9ab9587ccf5ae1
Sample First <132287607+SampleFirst@users.noreply.github.com>
2023-10-09 01:44:33 +0530

    Update geeta.py

--- plugins/helper/geeta.py
+++ plugins/helper/geeta.py
@@ -1,37 +1,28 @@
 from pyrogram import Client, filters
-from bs4 import BeautifulSoup
 import requests
-from info import *
+from info import ADMINS
 
-@Client.on_message(filters.command("geetaverse"))
+
+# Define the command handler
+@Client.on_message(filters.command("geetaverse") & filters.user(ADMINS))
 def send_geeta_verse(client, message):
-    verse_with_meaning = get_bhagavad_gita_verse()
-    message.reply_text(verse_with_meaning)
+    # Free Geeta API endpoint
+    api_url = "https://bhagavadgitaapi.in/api/v1/verse"
 
-def get_bhagavad_gita_verse():
-    url = "https://www.holy-bhagavad-gita.org/"
-    
-    # Set a user agent in the request headers
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
-    
     try:
-        response = requests.get(url, headers=headers)
-        response.raise_for_status()  # Raise an HTTPError for bad responses
+        # Make a GET request to the API
+        response = requests.get(api_url)
+        response.raise_for_status()  # Raise an error for bad responses
 
-        soup = BeautifulSoup(response.text, "html.parser")
+        # Parse the JSON response
+        verse_data = response.json()
 
-        # Extracting verse and meaning
-        verse_element = soup.find("div", class_="verse")
-        meaning_element = soup.find("div", class_="verse-meaning")
+        # Extract the verse
+        verse_text = verse_data["data"]["verse"]
 
-        # Check if elements are found before accessing their text attributes
-        if verse_element and meaning_element:
-            verse = verse_element.text.strip()
-            meaning = meaning_element.text.strip()
-            return f"{verse}\n\n{meaning}"
-        else:
-            return "Verse or meaning not found on the webpage."
+        # Send the verse as a reply
+        message.reply_text(verse_text)
 
-    except requests.RequestException as e:
-        return f"Error fetching data from the webpage: {str(e)}"
-
+    except Exception as e:
+        # Handle any errors that might occur during the process
+        message.reply_text(f"Error fetching Bhagavad Gita verse: {str(e)}")
