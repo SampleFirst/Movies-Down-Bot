a6cc0b0ee694af65bb52ed4eb69adb429c69ed96
Sample First <132287607+SampleFirst@users.noreply.github.com>
2023-10-09 02:17:26 +0530

    Update geeta.py

--- plugins/helper/geeta.py
+++ plugins/helper/geeta.py
@@ -2,7 +2,6 @@ from pyrogram import Client, filters
 import requests
 from info import ADMINS
 
-
 # Define the command handler
 @Client.on_message(filters.command("geetaverse") & filters.user(ADMINS))
 def send_geeta_verse(client, message):
@@ -14,15 +13,25 @@ def send_geeta_verse(client, message):
         response = requests.get(api_url)
         response.raise_for_status()  # Raise an error for bad responses
 
-        # Parse the JSON response
-        verse_data = response.json()
+        # Check if the response contains valid JSON
+        if response.headers.get("content-type") == "application/json":
+            # Parse the JSON response
+            verse_data = response.json()
 
-        # Extract the verse
-        verse_text = verse_data["data"]["verse"]
+            # Extract the verse
+            verse_text = verse_data["data"]["verse"]
 
-        # Send the verse as a reply
-        message.reply_text(verse_text)
+            # Send the verse as a reply
+            message.reply_text(verse_text)
+        else:
+            # If the response is not JSON, handle it accordingly
+            message.reply_text("Unexpected response format: Not a JSON")
+
+    except requests.exceptions.HTTPError as http_err:
+        # Handle HTTP errors (e.g., 404 Not Found, 500 Internal Server Error)
+        message.reply_text(f"HTTP error occurred: {http_err}")
 
     except Exception as e:
-        # Handle any errors that might occur during the process
+        # Handle any other errors that might occur during the process
         message.reply_text(f"Error fetching Bhagavad Gita verse: {str(e)}")
+
